42428ba327e23f0dcfbcb3ebb0f58d0e05badb08
Violettissima <violetta@cammeresi.com>
2026-01-13 09:28:39 -0800

    Create VioletsClass.java
    Created a program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VioletsClass.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VioletsClass extends OpMode {
+
+    @Override
+    public void init(){
+        telemetry.addData("Violet's", "Class!");
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
